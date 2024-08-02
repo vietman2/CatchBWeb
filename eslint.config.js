1b2734b795b44126d1bf69bd13f22f153379edc6
vietman2 <vietman0@gmail.com>
2024-08-02 12:31:25 +0900

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,14 @@
 const tseslint = require("typescript-eslint");
 const globals = require("globals");
-const reactPlugin = require("eslint-plugin-react");
+const react = require("eslint-plugin-react");
 
 module.exports = [
   {
     files: ["src/**/*.{js,ts,jsx,tsx}"],
-    ignores: [
-      "**/*.d.ts",
-      "**/*.config.js",
-      "node_modules/**",
-    ],
+    ignores: ["**/*.d.ts", "**/*.config.js", "node_modules/**"],
+    plugins: {react},
     languageOptions: {
-      ...reactPlugin.configs.flat.recommended.languageOptions,
+      ...react.configs.flat.recommended.languageOptions,
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -23,10 +20,8 @@ module.exports = [
       },
     },
     rules: {
-      "react/react-in-jsx-scope": "off",
       semi: "error",
     },
   },
-  reactPlugin.configs.flat.recommended,
   ...tseslint.configs.recommended,
 ];
