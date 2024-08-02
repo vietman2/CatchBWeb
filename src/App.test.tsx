97de6159b8325d4db0c1464690681360aced6a80
vietman2 <vietman0@gmail.com>
2024-08-02 12:42:33 +0900

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("react-scroll", () => ({
+  Element: "Element",
+}));
+jest.mock("./containers/Header", () => "Header");
+jest.mock("./containers/AppIntro", () => "AppIntro");
+jest.mock("./containers/Events", () => "Events");
+jest.mock("./containers/Notices", () => "Notices");
+jest.mock("./containers/PreOrder", () => "PreOrder");
+jest.mock("./containers/HowToUse", () => "HowToUse");
+jest.mock("./containers/Footer", () => "Footer");
+jest.mock("./containers/NavBar", () => "NavBar");
+
+describe("App", () => {
+  it("renders learn react link", () => {
+    render(<App />);
+  });
 });
